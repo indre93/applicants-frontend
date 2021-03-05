34a4b6a1f24d455c3525aa18ed10b1b11c4fc370
Indre Bakeer <indre.bakeer@gmail.com>
2021-03-05 13:39:54 -0600

    add handleClick to remove applicant clicked on using applicant id

--- src/components/Applicant.js
+++ src/components/Applicant.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import { useHistory } from 'react-router-dom';
 
 // Applicant component is responsible for rendering applicant's properties
-const Applicant = (props) => {
+const Applicant = ({ applicant }) => {
+  const history = useHistory();
+
+  const handleClick = () => {
+    fetch(`http://localhost:8000/applicants/${applicant.id}`, {
+      method: "DELETE"
+    }).then(() => {
+      history.push("/");
+    });
+  };
+
   return (
     <tr>
-      <td>{props.applicant.firstName}</td>
-      <td>{props.applicant.lastName}</td>
-      <td>{props.applicant.occupation}</td>
-      <td>{props.applicant.ssn}</td>
-      <td><Button variant="secondary" size="sm">Update</Button></td>
-      <td><Button variant="danger" size="sm">Remove</Button></td>
+      <td>{applicant.firstName}</td>
+      <td>{applicant.lastName}</td>
+      <td>{applicant.occupation}</td>
+      <td>{applicant.ssn}</td>
+      <td>
+        <Button variant="secondary">Update</Button>
+      </td>
+      <td>
+        <Button onClick={handleClick} variant="danger" >Remove</Button>
+      </td>
     </tr>
   );
 };

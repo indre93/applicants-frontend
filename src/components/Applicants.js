092e7ec8a886d8a3d9c4b51b1c59e2a31f470e71
Indre Bakeer <indre.bakeer@gmail.com>
2021-03-04 23:56:55 -0600

    create Applicants component to render list of applicants

---
+++ src/components/Applicants.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Applicants component is responsible for rendering list of applicants
+const Applicants = (props) => {
+  return (
+    <div></div>
+  );
+};
+
+export default Applicants;

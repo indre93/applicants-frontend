808bb05de9f5a0be9b25c1596690641c12a8974e
Indre Bakeer <indre.bakeer@gmail.com>
2021-03-05 00:07:48 -0600

    import custom hook useFetch and use it to fetch json-server fake data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import Applicants from "./components/Applicants";
+import useFetch from './useFetch';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+
+// Because props are only passed from top to bottom in a component tree, App component will handle state and pass it as props to child components.
+
 function App() {
+  const { data, error } = useFetch("http://localhost:8000/applicants");
+
   return (
     <div className="App">
       <div className="App-header">
         <h1>Applicants</h1>
       </div>
+
+      {/* Pass data as props to Applicants component */}
+      {/* Use JS logical && operator to conditionally include an element */}
+      {/* If applicants state has a value and it evaluates to true, then the element after && will appear in the output */}
+      <Container fluid>
+        {data && <Applicants applicants={data} />}
+        {error && <div>{error}</div>}
+      </Container>
     </div>
   );
 }

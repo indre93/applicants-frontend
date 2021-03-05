76d1d762de965d41f478eddeb7d9a4d39d3eeaa9
Indre Bakeer <indre.bakeer@gmail.com>
2021-03-05 00:12:55 -0600

    import Table from react-bootstrap to display list of applicants

--- src/components/Applicants.js
+++ src/components/Applicants.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Table from 'react-bootstrap/Table';
 
 // Applicants component is responsible for rendering list of applicants
 const Applicants = (props) => {
   return (
-    <div></div>
+    // Import Table from react-bootstrap for readability and qucik styling
+    <Table hover responsive>
+      <thead>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Occupation</th>
+          <th>SSN</th>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+
+      </tbody>
+    </Table>
   );
 };
 

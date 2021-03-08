dfd34f2230152db57eef3e6beb2fcb8f2351637b
Indre Bakeer <indre.bakeer@gmail.com>
2021-03-08 00:22:55 -0600

    remove useFetch custom hook

--- src/App.js
+++ src/App.js
@@ -9,26 +9,15 @@ import EditApplicant from "./components/EditApplicant";
 // Add Route to render the specified component when the path matches the current URL.
 // Add Switch to render the first Route that matches the url
 function App() {
-  // Desctructure the useFetch component
-  const { data, error, isPending } = useFetch("http://localhost:8000/applicants");
-
   return (
     <React.Fragment>
       <div className="App">
         <div className="App-header">
           <h1>Applicants</h1>
         </div>
-
         <Container>
           <Switch>
-            <Route exact path="/">
-              <ApplicantList
-                applicants={applicants}
-                error={error}
-                isLoading={isLoading}
-                handleDelete={handleDelete}
-              />
-            </Route>
+            <Route exact path="/" component={ApplicantList} />
             <Route path="/create" component={AddApplicant} />
             <Route path="/edit/:id" component={EditApplicant} />
           </Switch>
